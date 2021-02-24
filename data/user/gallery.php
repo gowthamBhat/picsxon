6e79661e06b21f592e236fe625c94839354978eb
Gowtham  H D <gowthambhat793@gmail.com>
2021-02-24 16:19:20 +0530

    Pagination-->Done

--- data/user/gallery.php
+++ data/user/gallery.php
@@ -21,126 +21,172 @@
 </head>
 
 <body>
-    <div class="title-main">
 
-        <a href="http://localhost/dogger/index.html" id="anc">
-            <p id="dogger">PicsXon</p>
+    <div class="title-main" style="width:fit-content;height:fit-content">
+
+        <a href="http://localhost/picsxon/index.html" id="anc">
+            <p id="main-title">PicsXon</p>
         </a>
 
 
     </div>
+    <div class="col">
+        <div class="row">
+            <div id="img-conatainer">
+
+                <?php
+                //connecting to database
+                $con = mysqli_connect("localhost", "root", "", "picsxon") or die("con Error " . mysqli_error($con));
 
 
 
-    <div id="img-conatainer">
-
-        <?php
-
-        $con = mysqli_connect("localhost", "root", "", "picsxon") or die("con Error " . mysqli_error($con));
-
-
-
-        //photo upload time indicator function
-        function timeAgo($time_ago)
-        {
-            $time_ago = strtotime($time_ago);
-            $cur_time   = time();
-            $time_elapsed   = $cur_time - $time_ago;
-            $seconds    = $time_elapsed;
-            $minutes    = round($time_elapsed / 60);
-            $hours      = round($time_elapsed / 3600);
-            $days       = round($time_elapsed / 86400);
-            $weeks      = round($time_elapsed / 604800);
-            $months     = round($time_elapsed / 2600640);
-            $years      = round($time_elapsed / 31207680);
-            // Seconds
-            if ($seconds <= 60) {
-                return "just now";
-            }
-            //Minutes
-            else if ($minutes <= 60) {
-                if ($minutes == 1) {
-                    return "one minute ago";
-                } else {
-                    return "$minutes minutes ago";
+                //photo upload time indicator function
+                function timeAgo($time_ago)
+                {
+                    $time_ago = strtotime($time_ago);
+                    $cur_time   = time();
+                    $time_elapsed   = $cur_time - $time_ago;
+                    $seconds    = $time_elapsed;
+                    $minutes    = round($time_elapsed / 60);
+                    $hours      = round($time_elapsed / 3600);
+                    $days       = round($time_elapsed / 86400);
+                    $weeks      = round($time_elapsed / 604800);
+                    $months     = round($time_elapsed / 2600640);
+                    $years      = round($time_elapsed / 31207680);
+                    // Seconds
+                    if ($seconds <= 60) {
+                        return "just now";
+                    }
+                    //Minutes
+                    else if ($minutes <= 60) {
+                        if ($minutes == 1) {
+                            return "one minute ago";
+                        } else {
+                            return "$minutes minutes ago";
+                        }
+                    }
+                    //Hours
+                    else if ($hours <= 24) {
+                        if ($hours == 1) {
+                            return "an hour ago";
+                        } else {
+                            return "$hours hrs ago";
+                        }
+                    }
+                    //Days
+                    else if ($days <= 7) {
+                        if ($days == 1) {
+                            return "yesterday";
+                        } else {
+                            return "$days days ago";
+                        }
+                    }
+                    //Weeks
+                    else if ($weeks <= 4.3) {
+                        if ($weeks == 1) {
+                            return "a week ago";
+                        } else {
+                            return "$weeks weeks ago";
+                        }
+                    }
+                    //Months
+                    else if ($months <= 12) {
+                        if ($months == 1) {
+                            return "a month ago";
+                        } else {
+                            return "$months months ago";
+                        }
+                    }
+                    //Years
+                    else {
+                        if ($years == 1) {
+                            return "one year ago";
+                        } else {
+                            return "$years years ago";
+                        }
+                    }
                 }
-            }
-            //Hours
-            else if ($hours <= 24) {
-                if ($hours == 1) {
-                    return "an hour ago";
-                } else {
-                    return "$hours hrs ago";
+
+                //Results that allowed per page
+                $results_per_page = 8;
+
+                //Retreving all the data
+                $q = "SELECT * FROM pictures";
+
+                $res = mysqli_query($con, $q);
+
+                if (!$res) {
+                    echo "query error";
                 }
-            }
-            //Days
-            else if ($days <= 7) {
-                if ($days == 1) {
-                    return "yesterday";
+                //This will return the number of results in the table
+                $number_of_results = mysqli_num_rows($res);
+
+                // determine number of total pages available
+                $number_of_pages = ceil($number_of_results / $results_per_page);
+
+                // determine which page number visitor is currently on
+                if (!isset($_GET['page'])) {
+                    $page = 1;
                 } else {
-                    return "$days days ago";
+                    $page = $_GET['page'];
                 }
-            }
-            //Weeks
-            else if ($weeks <= 4.3) {
-                if ($weeks == 1) {
-                    return "a week ago";
-                } else {
-                    return "$weeks weeks ago";
+
+                // determine the sql LIMIT starting number for the results on the displaying page
+                $this_page_first_result = ($page - 1) * $results_per_page;
+
+                $sql = ' SELECT * FROM pictures ORDER BY like_count DESC LIMIT ' . $this_page_first_result . ',' .  $results_per_page;
+                $result = mysqli_query($con, $sql);
+
+                while ($row = mysqli_fetch_array($result)) {
+
+
+                    echo "<div class='card' style='width: 18rem;'> ";
+                    echo "<img class='card-img-top' src='../admin/images/" . $row['path'] . "' alt='Card image cap'> "; //bootstrap card-view is used
+                    echo " <div class='card-body'> ";
+                    echo " <h5 class='card-title'>" . $row['picture_name'] . "</h5> ";
+                    echo " <p class='card-text'>Category:" . $row['category'] . "&nbsp  like:" . $row['like_count'] . "&nbsp  dislike:" . $row['dislike_count'] . "</p> ";
+
+                    echo timeAgo($row['timestamp']); //calling timeago function 
+
+                    echo "  </div> ";
+                    echo "  </div> ";
                 }
-            }
-            //Months
-            else if ($months <= 12) {
-                if ($months == 1) {
-                    return "a month ago";
-                } else {
-                    return "$months months ago";
-                }
-            }
-            //Years
-            else {
-                if ($years == 1) {
-                    return "one year ago";
-                } else {
-                    return "$years years ago";
-                }
-            }
-        }
+
+
+                ?>
 
 
 
 
+            </div>
+        </div>
 
+        <div class="row">
+            <div class="paginationBar">
+                <nav aria-label="...">
+                    <ul class="pagination pagination-lg">
+                        <?php
+                        // display the links to the pages
+                        for ($page = 1; $page <= $number_of_pages; $page++) {
+                            echo  "<li class='page-item'>";
+                            echo '<a class="page-link" href="gallery.php?page=' . $page . '" tabindex="-1">' . $page . '</a>';
+                            echo " </li>";
+                            // echo '<a class="" href="gallery.php?page=' . $page . '">' . $page . '</a> ';
+                        }
 
+                        ?>
+                    </ul>
+                </nav>
+            </div>
 
-
-
-        $q = "SELECT * FROM pictures ORDER BY like_count DESC";
-
-        $res = mysqli_query($con, $q);
-
-        if (!$res) {
-            echo "query error";
-        }
-
-        while ($row = mysqli_fetch_array($res)) {
-
-
-            echo "<div class='card' style='width: 18rem;'> ";
-            echo "<img class='card-img-top' src='../admin/images/" . $row['path'] . "' alt='Card image cap'> "; //bootstrap card-view is used
-            echo " <div class='card-body'> ";
-            echo " <h5 class='card-title'>" . $row['picture_name'] . "</h5> ";
-            echo " <p class='card-text'>Breed:" . $row['category'] . "&nbsp &nbsp  like:" . $row['like_count'] . "&nbsp  dislike:" . $row['dislike_count'] . "</p> ";
-
-            echo timeAgo($row['timestamp']); //calling timeago function 
-
-            echo "  </div> ";
-            echo "  </div> ";
-        }
-        ?>
-
-
+        </div>
     </div>
+
+
+
+
+
+
 
 </body>
 
