f69ae1140bedeff9d5e93d2c365739913a0612ad
Gowtham  H D <gowthambhat793@gmail.com>
2021-03-19 14:44:29 +0530

    moving js code in html to separate js file

--- data/admin/adminDashboard.php
+++ data/admin/adminDashboard.php
@@ -63,62 +63,6 @@
         </div>
     </div>
 
-    <script>
-        var user_table = document.getElementById("user-table");
-
-        window.onload = fetchCall();
-
-        function fetchCall() {
-            fetch(`userList.php`)
-                .then(response => response.json())
-                .then(data => {
-                    fetchDataHandler(data)
-                });
-        }
-
-        function fetchDataHandler(data) {
-            let userListOutPut = '';
-            console.log(data);
-
-            if (data == 0) {
-                userListOutPut = `<thead>
-            <tr>
-                <th><h3>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp No Users Found </h3></th>
-               
-            </tr>
-        </thead>`;
-            } else {
-                userListOutPut = `<thead>
-            <tr>
-                <th>&nbsp&nbsp&nbsp Name </th>
-                <th> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Email </th>
-                <th> </th>
-            </tr>
-        </thead>`;
-
-
-
-                for (let i = 0; i < data.length; i++) {
-                    userListOutPut += "<tr><td>" + data[i][1] + "</td> <td> &nbsp&nbsp&nbsp " + data[i][3] + "</td> <td> <button class='btn btn-danger btn-sm' onclick=deleteUser(" + data[i][0] + ")>X</button></td> </tr>"
-                }
-
-            }
-
-            user_table.innerHTML = userListOutPut;
-
-
-        }
-
-        function deleteUser(id) {
-
-            fetch(`deleteUser.php?id=${id}`)
-                .then(response => response.json())
-                .then(data => {
-                    fetchCall();
-                });
-        }
-    </script>
-
 </body>
 
 </html>
--- js/adminDashboardScript.js
+++ js/adminDashboardScript.js
@@ -7,26 +7,50 @@ user.addEventListener('click', function (event) {
 
 
 //* Fetch call scripts
+var user_table = document.getElementById("user-table");
 
-// window.onload = function () {
+window.onload = fetchCall();
 
-//     fetch(`userList.php`)
-//         .then(response => response.json())
-//         .then(data => {
-//             fetchDataHandler(data)
-//         });
-// }
+function fetchCall() {
+    fetch(`userList.php`)
+        .then(response => response.json())
+        .then(data => {
+            fetchDataHandler(data)
+        });
+}
 
-// function fetchDataHandler(data) {
-//     console.log(data);
+function fetchDataHandler(data) {
+    let userListOutPut = '';
+    console.log(data);
 
-// }
+    if (data == 0) {
+        userListOutPut = `<thead>
+    <tr>
+        <th><h3>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp No Users Found </h3></th>
+       
+    </tr>
+</thead>`;
+    } else {
+        userListOutPut = `<thead>
+    <tr>
+        <th>&nbsp&nbsp&nbsp Name </th>
+        <th> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Email </th>
+        <th> </th>
+    </tr>
+</thead>`;
 
-// function deleteUser(id) {
+        for (let i = 0; i < data.length; i++) {
+            userListOutPut += "<tr><td>" + data[i][1] + "</td> <td> &nbsp&nbsp&nbsp " + data[i][3] + "</td> <td> <button class='btn btn-danger btn-sm' onclick=deleteUser(" + data[i][0] + ")>X</button></td> </tr>"
+        }
+    }
+    user_table.innerHTML = userListOutPut;
+}
 
-//     fetch(`deleteUser.php?id=${id}`)
-//         .then(response => response.json())
-//         .then(data => {
-//             location.reload();
-//         });
-// }
+function deleteUser(id) {
+
+    fetch(`deleteUser.php?id=${id}`)
+        .then(response => response.json())
+        .then(data => {
+            fetchCall();
+        });
+}
